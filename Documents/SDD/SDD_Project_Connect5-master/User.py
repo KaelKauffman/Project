29a96dbb7469b721c402c1d276f7149daa8230f3
KauffK <kauffk@rpi.edu>
2019-03-19 15:34:39 -0400

    Minor updates to User.py, now uses Requests and python 3.6 print statements

--- Documents/SDD/SDD_Project_Connect5-master/User.py
+++ Documents/SDD/SDD_Project_Connect5-master/User.py
@@ -1,6 +1,4 @@
-import simplejson as json
 import requests
-import urllib
 
 steam_id = '76561197960434622'
 steam_token = '7C8A11057FBAB292E6E6B0AF1F6E9D19'
@@ -13,16 +11,47 @@ class User:
         self.gameTags = []
     def getPlayerName(steamID):
         summary_url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+steam_token+"&steamids="+steamID
-        steam_data = urllib.urlopen(summary_url)
-        player = json.loads(steam_data.read())
+        player = requests.get(summary_url).json()
+        try:
+            player = player['response']
+            player = player['players'][0]
+            player = player['personaname']
+            return player
+        except:
+            return "Error: Player Name No Response"
+        
+    def getPlayedGames(steamID):
+        recent_games_url = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key="+steam_token+"&steamid="+steamID+"&format=json"
+        games = requests.get(recent_games_url).json()
+        try:
+            games = games['response']
+            games = games['games']
+            games_list = []
+            for game in games:
+                game_id = game['appid']
+                game_name = game['name']
+                play_time = game['playtime_2weeks']
+                game_info = game_id,game_name,play_time
+                games_list.append(game_info)
+            return games_list
+        except:
+            return []
+    
+def getPlayerName(steamID):
+    summary_url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+steam_token+"&steamids="+steamID
+    player = requests.get(summary_url).json()
+    try:
         player = player['response']
         player = player['players'][0]
         player = player['personaname']
         return player
-    def getPlayedGames(steamID):
-        recent_games_url = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key="+steam_token+"&steamid="+steamID+"&format=json"
-        steam_data = urllib.urlopen(recent_games_url)
-        games = json.loads(steam_data.read())
+    except:
+        return "Error: Player Name No Response"
+
+def getPlayedGames(steamID):
+    recent_games_url = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key="+steam_token+"&steamid="+steamID+"&format=json"
+    games = requests.get(recent_games_url).json()
+    try:
         games = games['response']
         games = games['games']
         games_list = []
@@ -32,50 +61,25 @@ class User:
             play_time = game['playtime_2weeks']
             game_info = game_id,game_name,play_time
             games_list.append(game_info)
-        return games_list    
-    
-def getPlayerName(steamID):
-    summary_url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key="+steam_token+"&steamids="+steamID
-    steam_data = urllib.urlopen(summary_url)
-    player = json.loads(steam_data.read())
-    player = player['response']
-    player = player['players'][0]
-    player = player['personaname']
-    print player
-
-
-def getPlayedGames(steamID):
-    recent_games_url = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key="+steam_token+"&steamid="+steamID+"&format=json"
-    steam_data = urllib.urlopen(recent_games_url)
-    games = json.loads(steam_data.read())
-    games = games['response']
-    games = games['games']
-    games_list = []
-    for game in games:
-        game_id = game['appid']
-        game_name = game['name']
-        play_time = game['playtime_2weeks']
-        game_info = game_id,game_name,play_time
-        games_list.append(game_info)
-    return games_list
+        return games_list
+    except:
+        return []
 
 def getGenres(gameID):
     genre_url = "https://steamspy.com/api.php?request=appdetails&appid="+str(gameID)
     check = requests.get(genre_url)
     if(check.status_code!=200):
         return None
-    steam_data = urllib.urlopen(genre_url)
-    genre = json.loads(steam_data.read())
-    genre = genre ['genre']
+    genre = check.json()
+    genre = genre['genre']
     genre_list = [x.strip() for x in genre.split(',')]
-    return map(str,genre_list)
+    return genre_list
 
 def getRelatedGames(genreList):
     related_list = []
     bad_list = []
     apps_url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
-    steam_data = urllib.urlopen(apps_url)
-    apps = json.loads(steam_data.read())
+    apps = requests.get(apps_url).json()
     apps = apps['applist']
     apps = apps['apps']
     for app in apps:
@@ -89,11 +93,12 @@ def getRelatedGames(genreList):
             app_name = app['name']
             app_info = app_id,app_name,intersection
             related_list.append(app_info)
-    print related_list
-    print
-    print bad_list
+    print(related_list)
+    print()
+    print(bad_list)
     
     
-a = getGenres("7730") 
-print getRelatedGames(a)
+#a = getGenres("427520")
+#print(a)
+#print(getRelatedGames(a))
 
