058f8882e8127a4f05255a69d0450cffbb1345a2
Michelle Zhou <michellezhou71@gmail.com>
2019-03-24 01:48:48 -0400

    Update GUI.py
    Added homepage graphics. Updated functions in game recommendation panel. General code maintenance.

--- Documents/SDD/SDD_Project_Connect5-master/GUI.py
+++ Documents/SDD/SDD_Project_Connect5-master/GUI.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import os
 
+## Module to connect to public Steam profile 
 def connect_to_steam():
 	filewin = Toplevel(root)
 	filewin.title("Connect to Steam.")
@@ -9,6 +10,9 @@ def connect_to_steam():
 	Label(filewin, text='Connect to your public Steam account here.').pack(padx=30, pady=30)
 	Button(filewin, text= "Login here").pack()
 
+## Module to switch current user. Currently uses a username/password system. 
+## Want to implement a dropdown menu for users to get rid of password authentication;
+## Not sure about security for that choice.
 def switch_user():
 	filewin = Toplevel(root)
 	filewin.title("Change User")
@@ -43,50 +47,59 @@ def switch_user():
 	# dropdown = OptionMenu(mainframe, tkvar, *choices)
 	# dropdown.grid(row=2, column=1)
 
+## Module to generate game recommendations based on 2 parameters, by type or by name.
 def generate_recommendation():
 	TYPES = ['Action', 'Adventure', 'Casual', 'Indie', 'Massively Multiplayer', 'Racing', 'RPG', 'Simulation', 'Sports', 'Strategy']
-	def types():
+			
+	## Module to generate game recommendations by type.
+	def by_types():
 		filewin1 = Toplevel(filewin)
 		filewin1.title("Recommend by Types")
+		#Obtain user input types.
 		vars=[]
 		for type in TYPES:
 			var = IntVar()
 			chk = Checkbutton(filewin1, text=type, variable=var)
 			chk.pack(side=LEFT, anchor=W, expand=YES)
 			vars.append(var)
+		
+		def submit(): print("Selection submitted")
+		def clear(): print("Selection cleared")
+		
 		Button(filewin1, text="Submit", command=submit).pack(side=RIGHT)
 		Button(filewin1, text="Clear", command=clear).pack(side=RIGHT)
 
-
-	def names():
+	## Module to generate game recommendations by game names. Incomplete.
+	def by_names():
 		os.system('python3 Search.py')
+		
+		
 	filewin = Toplevel(root)
-	filewin.title("Game Recommendation")
-	type_button = Button(filewin, text="Recommend by Types", width=20, command=types)
+	filewin.title("Game Recommendations")
+	type_button = Button(filewin, text="Recommend by Types", width=20, command=by_types)
 	type_button.grid(row=0,column=0)
-	name_button = Button(filewin, text="Recommend by Names", width=20, command=names)
+	name_button = Button(filewin, text="Recommend by Names", width=20, command=by_names)
 	name_button.grid(row=0, column=1)
 
+## Module to conduct game search and bring user to the specified game page.
 def search_game():
 	os.system('python3 Search.py')
 
+## Module to view user's wishlist.
 def wishlist():
 	Label(root, text="See wishlist").pack()
 
-def gamewatch():
+## Module to check price of one game.
+def pricecheck():
 	os.System("pyhon3 Search.py")
 
+## See rankings by account value.
 def rank_by_account_value():
 	Label(root, text="See rankings by account value").pack()
 
+## See rankings by account level.
 def rank_by_account_level():
 	Label(root, text="See rankings by account level").pack()
-
-def submit():
-	Label(root, text="Submit Succeed!").pack()
-
-def clear():
-	Label(root, text="Clear Succeed!").pack()
 
 def make_menus():
 	menu = Menu(root)
@@ -107,7 +120,7 @@ def make_menus():
 	pricewarch_menu = Menu(menu, tearoff=0)
 	menu.add_cascade(label="PriceWatch", menu=pricewarch_menu)
 	pricewarch_menu.add_command(label="Wishlist", command=wishlist)
-	pricewarch_menu.add_command(label="Gamewatch", command=gamewatch)
+	pricewarch_menu.add_command(label="Price Check", command=pricecheck)
 
 	ranking_menu = Menu(menu, tearoff=0)
 	menu.add_cascade(label="Ranking", menu=ranking_menu)
@@ -117,6 +130,10 @@ def make_menus():
 
 root = Tk()
 root.title("SteamRush")
-root.geometry("800x600")
+root.geometry("400x300")
+root.configure(background='white')
 make_menus()
+steam_icon = PhotoImage(file= "images/steam_icon.gif")
+Label(root, bg='white', image=steam_icon).pack()
+Label(root, text="Welcome to SteamRush!", font=("TKHeadingFont", 26), bg='white').pack()
 root.mainloop()
