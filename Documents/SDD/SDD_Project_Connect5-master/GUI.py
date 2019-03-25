fef9afe2f707c37454d64adfebe13ce730b82362
KauffK <kauffk@rpi.edu>
2019-03-25 16:16:26 -0400

    Backend GUI updates

--- Documents/SDD/SDD_Project_Connect5-master/GUI.py
+++ Documents/SDD/SDD_Project_Connect5-master/GUI.py
@@ -14,58 +14,58 @@ steam_user = SteamUser(userFile="User_Data_Cache.txt")
 
 ## Module to connect to public Steam profile 
 def connect_to_steam():
-	filewin = Toplevel(root)
-	filewin.title("Connect to Steam.")
-	
-	#Have popup window follow parent object placement.
-	x=root.winfo_rootx()
-	y=root.winfo_rooty()
-	geom="+%d+%d" % (x,y)
-	filewin.geometry(geom)
+        filewin = Toplevel(root)
+        filewin.title("Connect to Steam.")
+        
+        #Have popup window follow parent object placement.
+        x=root.winfo_rootx()
+        y=root.winfo_rooty()
+        geom="+%d+%d" % (x,y)
+        filewin.geometry(geom)
 
-	description ='Connect to your public Steam account here by entering your SteamID'
-	Label(filewin, text=description).pack(padx=30, pady=3)
-	steamID = Entry(filewin)
-	steamID.pack(padx=3, pady=3, ipadx=10, ipady=4)
+        description ='Connect to your public Steam account here by entering your SteamID'
+        Label(filewin, text=description).pack(padx=30, pady=3)
+        steamID = Entry(filewin)
+        steamID.pack(padx=3, pady=3, ipadx=10, ipady=4)
 
-	def log_in():
-		print(steamID.get())
-		steam_user.loginSteamID(steamID.get())
+        def log_in():
+                print(steamID.get())
+                steam_user.loginSteamID(steamID.get())
 
-	Button(filewin, text= "Login here", command=log_in).pack()
+        Button(filewin, text= "Login here", command=log_in).pack()
 
 ## Module to switch current user. Currently uses a username/password system. 
 ## Want to implement a dropdown menu for users to get rid of password authentication;
 ## Not sure about security for that choice.
 def switch_user():
-	filewin = Toplevel(root)
-	filewin.title("Users")
-	
-	x=root.winfo_rootx()
-	y=root.winfo_rooty()
-	geom="+%d+%d" % (x,y)
-	filewin.geometry(geom)
-	#Suppose every account that ever login is stored in users
-	users=["Andrew", "Kael", "Michelle", "Yao", "Yiwei"]
-	row_num=1
-	var=IntVar()
-	var.set(1)
-	for user in users:
-		r = Radiobutton(filewin, text=user, variable=var, value=row_num)
-		r.pack(anchor=W)
-		row_num+=1
-	# #Username and password labels
-	# Label(filewin, text='Username').grid(row=0)
-	# Label(filewin, text='Password').grid(row=1)
+        filewin = Toplevel(root)
+        filewin.title("Users")
+        
+        x=root.winfo_rootx()
+        y=root.winfo_rooty()
+        geom="+%d+%d" % (x,y)
+        filewin.geometry(geom)
+        #Suppose every account that ever login is stored in users
+        users=["Andrew", "Kael", "Michelle", "Yao", "Yiwei"]
+        row_num=1
+        var=IntVar()
+        var.set(1)
+        for user in users:
+                r = Radiobutton(filewin, text=user, variable=var, value=row_num)
+                r.pack(anchor=W)
+                row_num+=1
+        # #Username and password labels
+        # Label(filewin, text='Username').grid(row=0)
+        # Label(filewin, text='Password').grid(row=1)
 
-	# #Entry fields for username, password
-	# e1 = Entry(filewin).grid(row=0,column=1, padx=3)
-	# e2 = Entry(filewin, show='*').grid(row=1, column=1, padx=3)
-	
-	# #Keep user logged in
-	# Checkbutton(filewin, text = "Keep Me Logged In").grid(columnspan=2)
-	# #Submit
-	# Button(filewin, text= "Login!", activebackground='pink1').grid(columnspan=2, pady=3)
+        # #Entry fields for username, password
+        # e1 = Entry(filewin).grid(row=0,column=1, padx=3)
+        # e2 = Entry(filewin, show='*').grid(row=1, column=1, padx=3)
+        
+        # #Keep user logged in
+        # Checkbutton(filewin, text = "Keep Me Logged In").grid(columnspan=2)
+        # #Submit
+        # Button(filewin, text= "Login!", activebackground='pink1').grid(columnspan=2, pady=3)
 
 
 # Return popup window of game recommendations given a game.
@@ -87,161 +87,162 @@ def get_game_rec(text):
 
 ## Module to generate game recommendations based on 2 parameters, by type or by name.
 def generate_recommendation():
-	TYPES = ['Action', 'Adventure', 'Casual', 'Indie', 'Massively Multiplayer', 'Racing', 'RPG', 'Simulation', 
-		 'Sports', 'Strategy']
-	## Module to generate game recommendations by type.
-	def by_types():
-		x=root.winfo_rootx()
-		y=root.winfo_rooty()
-		geom="+%d+%d" % (x,y)
-		filewin1 = Toplevel(root)
-		filewin1.title("Recommend by Types")
-		filewin1.geometry(geom)
-		#Obtain user input types.
-		vars=[]
-		checks=[]
-		row_num=1
-		#Output checkboxes in column format.
-		for type in TYPES:
-			var = IntVar()
-			chk = Checkbutton(filewin1, text=type, variable=var)
-			chk.grid(row=row_num, sticky=W)
-			row_num+=1
-			vars.append(var)
-		def submit():
-			checks.clear()
-			count=0
-			for i in vars:
-				if i.get() == 1 and TYPES[count] not in checks:
-					checks.append(TYPES[count])
-				count+=1
-			print(checks)
+        TYPES = ['Action', 'Adventure', 'Casual', 'Indie', 'Massively Multiplayer', 'Racing', 'RPG', 'Simulation', 
+                 'Sports', 'Strategy']
+        ## Module to generate game recommendations by type.
+        def by_types():
+                x=root.winfo_rootx()
+                y=root.winfo_rooty()
+                geom="+%d+%d" % (x,y)
+                filewin1 = Toplevel(root)
+                filewin1.title("Recommend by Types")
+                filewin1.geometry(geom)
+                #Obtain user input types.
+                vars=[]
+                checks=[]
+                row_num=1
+                #Output checkboxes in column format.
+                for type in TYPES:
+                        var = IntVar()
+                        chk = Checkbutton(filewin1, text=type, variable=var)
+                        chk.grid(row=row_num, sticky=W)
+                        row_num+=1
+                        vars.append(var)
+                def submit():
+                        checks.clear()
+                        count=0
+                        for i in vars:
+                                if i.get() == 1 and TYPES[count] not in checks:
+                                        checks.append(TYPES[count])
+                                count+=1
+                        print(checks)
 
-		def clear_checkbox():
-			for i in vars:
-				i.set(0)
-			checks.clear()
-			print(checks)
-		
-		Button(filewin1, text="Submit", activebackground='pink1', command=submit).grid(row=row_num, pady=3)
-		Button(filewin1, text="Clear", activebackground='pink1', command=clear_checkbox).grid(row=row_num+1)
+                def clear_checkbox():
+                        for i in vars:
+                                i.set(0)
+                        checks.clear()
+                        print(checks)
+                
+                Button(filewin1, text="Submit", activebackground='pink1', command=submit).grid(row=row_num, pady=3)
+                Button(filewin1, text="Clear", activebackground='pink1', command=clear_checkbox).grid(row=row_num+1)
 
-	## Module to generate game recommendations by game names. Incomplete.
-	def by_names():
-		filewin2 = Toplevel(root)
-		filewin2.title("Recommend by Names")
+        ## Module to generate game recommendations by game names. Incomplete.
+        def by_names():
+                filewin2 = Toplevel(root)
+                filewin2.title("Recommend by Names")
 
-		x=root.winfo_rootx()
-		y=root.winfo_rooty()
-		geom="+%d+%d" % (x,y)
-		filewin2.geometry(geom)
+                x=root.winfo_rootx()
+                y=root.winfo_rooty()
+                geom="+%d+%d" % (x,y)
+                filewin2.geometry(geom)
 
-		Search.SearchBox(filewin2, command=get_game_rec, placeholder="Enter game name").pack(pady=6, padx=3)
-		
-	## User game recommendation method selection: by type or by name.	
-	x=root.winfo_rootx()
-	y=root.winfo_rooty()
-	geom="+%d+%d" % (x,y)
-	filewin = Toplevel(root)
-	filewin.title("Game Recommendation")
-	filewin.geometry(geom)
-	type_button = Button(filewin, text="Recommend by Types", width=20, command=by_types)
-	type_button.grid(row=0,column=0)
-	name_button = Button(filewin, text="Recommend by Names", width=20, command=by_names)
-	name_button.grid(row=0, column=1)
+                Search.SearchBox(filewin2, command=get_game_rec, placeholder="Enter game name").pack(pady=6, padx=3)
+                
+        ## User game recommendation method selection: by type or by name.       
+        x=root.winfo_rootx()
+        y=root.winfo_rooty()
+        geom="+%d+%d" % (x,y)
+        filewin = Toplevel(root)
+        filewin.title("Game Recommendation")
+        filewin.geometry(geom)
+        type_button = Button(filewin, text="Recommend by Types", width=20, command=by_types)
+        type_button.grid(row=0,column=0)
+        name_button = Button(filewin, text="Recommend by Names", width=20, command=by_names)
+        name_button.grid(row=0, column=1)
 
 ## Module to view user's wishlist.
 def wishlist():
-	wishlist = [ steam_api.get_name(g_id) for g_id in steam_user.getDesiredGames() ]
-	raw_prices = [ itad_api.get_prices(itad_api.get_plain(g_id)) for g_id in steam_user.getDesiredGames() ]
+        wishlist = [ steam_api.get_name(g_id) for g_id in steam_user.getDesiredGames() ]
+        raw_prices = [ itad_api.get_prices(itad_api.get_plain(g_id)) for g_id in steam_user.getDesiredGames() ]
         revised_prices = []
-	for item in raw_prices:
+        for item in raw_prices:
                 if len(item) > 1:
                         s_p = ("Steam", 9999)
                         l_p = ("Steam", 9999)
                         for i in range(1, len(item)):
-                                if item[0] == "Steam":
-                                        s_p = item
-                                if (l_p[1] - item[1]) > 0.1:
-                                        l_p = item
+                                if item[i][0] == "Steam":
+                                        s_p = item[i]
+                                if (l_p[1] - item[i][1]) > 0.1:
+                                        l_p = item[i]
                         revised_prices.append([s_p, l_p])
                 else:
                         revised_prices.append([("Steam", -1), ("Steam", -1)])
 
-	filewin = Toplevel(root)
-	filewin.title("Wishlist")
 
-	x=root.winfo_rootx()
-	y=root.winfo_rooty()
-	geom="+%d+%d" % (x,y)
-	filewin.geometry(geom)
+        filewin = Toplevel(root)
+        filewin.title("Wishlist")
 
-	Label(filewin, text="Your wishlist is displayed below.", font=("", 10, "italic")).grid(pady=3, columnspan=4)
-	row_num = 2
-	for game in wishlist:
-		#Game name, price information, and current rating are displayed. Rating is optional.
-		Label(filewin, text=game, font="fixedsys 12 bold").grid(row=row_num, sticky=W, columnspan=4)
-		Label(filewin, text="Current Price: {}".format(9.99)).grid(row=row_num+1, columnspan=2, sticky=W, padx=8)
-		Label(filewin, text="Lowest Price: {}".format(9.99)).grid(row=row_num+2, columnspan=2, sticky=W, padx=8)
-		Label(filewin, text="Vendor: {}".format("Humble")).grid(row=row_num+3, columnspan=2, sticky=W, padx=12)
-		Label(filewin, text="").grid(row=row_num+4, columnspan=4)
+        x=root.winfo_rootx()
+        y=root.winfo_rooty()
+        geom="+%d+%d" % (x,y)
+        filewin.geometry(geom)
 
-		row_num += 5
+        Label(filewin, text="Your wishlist is displayed below.", font=("", 10, "italic")).grid(pady=3, columnspan=4)
+        row_num = 2
+        for g in range(len(wishlist)):
+                #Game name, price information, and current rating are displayed. Rating is optional.
+                Label(filewin, text=wishlist[g], font="fixedsys 12 bold").grid(row=row_num, sticky=W, columnspan=4)
+                Label(filewin, text="Current Steam Price: {}".format(revised_prices[g][0][1])).grid(row=row_num+1, columnspan=2, sticky=W, padx=8)
+                Label(filewin, text="Lowest Price: {}".format(revised_prices[g][1][1])).grid(row=row_num+2, columnspan=2, sticky=W, padx=8)
+                Label(filewin, text="Vendor: {}".format(revised_prices[g][1][0])).grid(row=row_num+3, columnspan=2, sticky=W, padx=12)
+                Label(filewin, text="").grid(row=row_num+4, columnspan=4)
+
+                row_num += 5
 
 ## Module to check price of one game.
 def pricecheck():
-	filewin = Toplevel(root)
-	filewin.title("Price Check")
+        filewin = Toplevel(root)
+        filewin.title("Price Check")
 
-	x=root.winfo_rootx()
-	y=root.winfo_rooty()
-	geom="+%d+%d" % (x,y)
-	filewin.geometry(geom)
+        x=root.winfo_rootx()
+        y=root.winfo_rooty()
+        geom="+%d+%d" % (x,y)
+        filewin.geometry(geom)
 
-	def command(text):
-		messagebox.showinfo("Search Results", "Searching for: %s"%text)
-	Search.SearchBox(filewin, command=command, placeholder="Enter game name").pack(pady=6, padx=3)
+        def command(text):
+                messagebox.showinfo("Search Results", "Searching for: %s"%text)
+        Search.SearchBox(filewin, command=command, placeholder="Enter game name").pack(pady=6, padx=3)
 
 ## See rankings by account value.
 def rank_by_account_value():
-	Label(root, text="See rankings by account value").pack()
+        Label(root, text="See rankings by account value").pack()
 
 ## See rankings by account level.
 def rank_by_account_level():
-	Label(root, text="See rankings by account level").pack()
+        Label(root, text="See rankings by account level").pack()
 
 ## Helper function to create menu bar and corresponding cascading options.
 def make_menus():
-	menu = Menu(root)
-	root.config(menu=menu)
+        menu = Menu(root)
+        root.config(menu=menu)
 
-	#User menu
-	user_menu = Menu(menu, tearoff=0)
-	user_menu.add_command(label="Connect to Steam", command=connect_to_steam)
-	user_menu.add_command(label="Change local user", command=switch_user)
-	user_menu.add_separator()
-	user_menu.add_command(label="Quit", command=root.quit)
-	menu.add_cascade(label="User", menu=user_menu)
+        #User menu
+        user_menu = Menu(menu, tearoff=0)
+        user_menu.add_command(label="Connect to Steam", command=connect_to_steam)
+        user_menu.add_command(label="Change local user", command=switch_user)
+        user_menu.add_separator()
+        user_menu.add_command(label="Quit", command=root.quit)
+        menu.add_cascade(label="User", menu=user_menu)
 
-	#Game menu
-	gamerec_menu = Menu(menu, tearoff=0)
-	gamerec_menu.add_command(label="Recommendation", command=generate_recommendation)
-	menu.add_cascade(label="Game", menu=gamerec_menu)
+        #Game menu
+        gamerec_menu = Menu(menu, tearoff=0)
+        gamerec_menu.add_command(label="Recommendation", command=generate_recommendation)
+        menu.add_cascade(label="Game", menu=gamerec_menu)
 
-	#Price watching menu
-	pricewatch_menu = Menu(menu, tearoff=0)	
-	pricewatch_menu.add_command(label="View Wishlist", command=wishlist)
-	pricewatch_menu.add_command(label="Price Check", command=pricecheck)
-	menu.add_cascade(label="Price Watch", menu=pricewatch_menu)
+        #Price watching menu
+        pricewatch_menu = Menu(menu, tearoff=0) 
+        pricewatch_menu.add_command(label="View Wishlist", command=wishlist)
+        pricewatch_menu.add_command(label="Price Check", command=pricecheck)
+        menu.add_cascade(label="Price Watch", menu=pricewatch_menu)
 
-	#Ranking menu
-	ranking_menu = Menu(menu, tearoff=0)
-	ranking_menu.add_command(label="By account value", command=rank_by_account_value)
-	ranking_menu.add_command(label="By account level", command=rank_by_account_level)
-	menu.add_cascade(label="Ranking", menu=ranking_menu)
+        #Ranking menu
+        ranking_menu = Menu(menu, tearoff=0)
+        ranking_menu.add_command(label="By account value", command=rank_by_account_value)
+        ranking_menu.add_command(label="By account level", command=rank_by_account_level)
+        menu.add_cascade(label="Ranking", menu=ranking_menu)
 
 def see_game_info(text_):
-	messagebox.showinfo("Search Results", text_)
+        messagebox.showinfo("Search Results", text_)
 
 
 
