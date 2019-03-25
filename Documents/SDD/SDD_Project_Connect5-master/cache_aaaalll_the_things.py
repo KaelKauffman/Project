aa11a9b9085e918515624dbbfad78eb3690cd7d1
KauffK <kauffk@rpi.edu>
2019-03-25 01:50:00 -0400

    added to cache

--- Documents/SDD/SDD_Project_Connect5-master/cache_aaaalll_the_things.py
+++ Documents/SDD/SDD_Project_Connect5-master/cache_aaaalll_the_things.py
@@ -3,28 +3,50 @@ import requests
 
 steam_api = SteamSpy_API_Caller(appFile="SteamSpy_App_Cache.txt", tagFile="SteamSpy_Tags_Cache.txt")
 
-
-result = requests.get("https://steamspy.com/api.php?request=top100in2weeks").json()
-top100 = list(result.keys())
-tag_pile = []
-for t in top100:
-    print(t)
-    t_tags = steam_api.get_tags(t)
-    tag_pile += t_tags
-
-check = steam_api.save_game_data_to_cache()
-print(check) #Saved to cache successfully.
+super_cache = steam_api.tag_data_cache
 
 frequency_list = {}
-for item in tag_pile:
-    frequency_list[item] = frequency_list.get(item, 0) + 1
+for tag in super_cache:
+    gamesList = list(super_cache[tag].keys())
+    for item in gamesList:
+        frequency_list[item] = frequency_list.get(item, 0) + 1
 
-all_tags = list(frequency_list.keys())
-print(len(all_tags))
-
-for t in all_tags:
-    flag = steam_api.load_tagged_games(t)
-    print(t + " : " + str(flag))
+all_games = list(frequency_list.keys())
+print("Total: " + str(len(all_games)))
+print(int(len(all_games)/20))
+for i in range(len(all_games)):
+    print(str(i+1) + " : " + steam_api.get_name(all_games[i]))
+    if i%(int(len(all_games)/20)) == 0:
+        check = steam_api.save_game_data_to_cache()
+        print("Saving... " + str(check)) #Saved to cache successfully.
 
 check = steam_api.save_game_data_to_cache()
-print(check) #Saved to cache successfully.
+print("Saving Final... " + str(check)) #Saved to cache successfully.
+
+##result = steam_api.app_data_cache
+##super_apps = list(result.keys())
+##tag_pile = []
+##for t in super_apps:
+##    #print(t)
+##    t_tags = steam_api.get_tags(t)
+##    tag_pile += t_tags
+##
+##check = steam_api.save_game_data_to_cache()
+##print(check) #Saved to cache successfully.
+##
+##frequency_list = {}
+##for item in tag_pile:
+##    frequency_list[item] = frequency_list.get(item, 0) + 1
+##
+##all_tags = list(frequency_list.keys())
+##print(len(all_tags))
+##
+##for i in range(len(all_tags)):
+##    junk = steam_api.get_games_with_tag(all_tags[i])
+##    print(str(i+1) + " " + all_tags[i] + " : " + str(len(junk) > 0))
+##    if i%(int(len(all_tags)/10)) == 0:
+##        check = steam_api.save_game_data_to_cache()
+##        print("Saving... " + str(check)) #Saved to cache successfully.
+##
+##check = steam_api.save_game_data_to_cache()
+##print("Saving Final... " + str(check)) #Saved to cache successfully.
